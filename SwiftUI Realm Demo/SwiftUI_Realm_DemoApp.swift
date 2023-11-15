5d418d874bbcfcd55980a74c5592cce1f6496a87
Chintanneosoft <chintan.rajgor@neosoftmail.com>
2023-11-15 11:15:55 +0530

    Initial Commit

---
+++ SwiftUI Realm Demo/SwiftUI_Realm_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Realm_DemoApp.swift
+//  SwiftUI Realm Demo
+//
+//  Created by Neosoft on 15/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Realm_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
